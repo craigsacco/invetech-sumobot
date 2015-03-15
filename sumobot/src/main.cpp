ddb631a99ee9852427c59e57644c2bd8d738838e
Craig Sacco <craig.sacco@gmail.com>
2015-03-15 18:40:16 +1100

    added some sample code to traverse root directory of SD card

--- sumobot/src/main.cpp
+++ sumobot/src/main.cpp
@@ -1,10 +1,26 @@
 #include <ch.h>
 #include <hal.h>
+#include <ff.h>
 
 int main()
 {
     chSysInit();
     halInit();
+
+    sdcStart(&SDCD1, NULL);
+
+    FRESULT res;
+    DIR dir;
+    FILINFO fileinfo;
+    res = f_opendir(&dir, "/");
+    if (res == FR_OK) {
+        for (;;) {
+            res = f_readdir(&dir, &fileinfo);
+            if (res != FR_OK || fileinfo.fname[0] == 0) {
+                break;
+            }
+        }
+    }
 
     return 0;
 }
